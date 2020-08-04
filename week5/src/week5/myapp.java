fe2f7b5beb5b9e88ca1d7a8c6708d5d2b468fea3
next7 <next7@192.168.1.119>
2020-08-04 14:38:24 +0700

    yanakorn tunbee

---
+++ week5/src/week5/myapp.java
@@ -0,0 +1,7 @@
+package week5;
+public class myapp {//class MyApp
+	public static void main(String[]args) {//method main
+		System.out.println("Java");//statement
+		System.out.println("Object");//statement
+	}
+}
